b82d88358b3e12a5dacd8da1dd9313658f0f76ed
varjo9 <79824903+varjo9@users.noreply.github.com>
2021-04-02 17:21:11 -0700

    Update SaveTab.java

--- SaveTab.java
+++ SaveTab.java
@@ -2,10 +2,13 @@
  * This is everything to do with the save tab of the GUI
  */
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.PrintWriter;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 
 public class SaveTab extends mainFrame implements ActionListener
 {
@@ -29,9 +32,60 @@ public class SaveTab extends mainFrame implements ActionListener
 	 */
 	public void actionPerformed(ActionEvent e) 
 	{
+		/*
+		 * Program opens up a FileChooser, where a user can choose a file location and give a file name.
+		 * If the user presses 'save' in the file explorer, the current array data will be written into a csv file 
+		 * at that location
+		 */
 		if(e.getSource() == save)
 		{
+			JFileChooser fileChooser = new JFileChooser();
+			String fileLocation = "";
 			
+			int response = fileChooser.showSaveDialog(null);
+			
+			if (response == JFileChooser.APPROVE_OPTION)	//if the user clicked 'save' in the file explorer (did not exit or close out of it)
+			{
+				//this section gets the file location chosen by the user and converts it into the fileLocation string
+				File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+				fileLocation = file.toString();
+				fileLocation = fileLocation + ".csv"; //add .csv to the fileLocation string to ensure that we're saving into a .csv file
+				
+				//this section of code takes the array data that we have and uses a StringBuilder to convert it into a format that a .csv file can take in (comma separated values)
+				// \r\n = newline
+				// ',' = next cell
+				try 
+				{
+					PrintWriter pw = new PrintWriter(new File(fileLocation));
+					StringBuilder sb = new StringBuilder();
+				
+					for(int i = 0; i < columnNames.length; i++) //append the column names first
+					{
+						sb.append(columnNames[i]);
+						sb.append(",");
+					}
+					
+					sb.append("\r\n");
+					
+					for(int j = 0; j < data.length; j++) //afterwards, append the actual data
+					{
+						for(int k = 0; k < data[0].length; k++)
+						{
+							sb.append(data[j][k]);
+							sb.append(",");
+						}
+						sb.append("\r\n");
+					}
+					
+					pw.write(sb.toString());
+					pw.close();
+					
+				}
+				catch(Exception g) {
+					
+				}
+			}
+				
 		}
 	}
 }
