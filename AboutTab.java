29d176777d214a95ad811c53398f82a008cbded4
varjo9 <79824903+varjo9@users.noreply.github.com>
2021-04-10 15:13:54 -0700

    Update AboutTab.java

--- AboutTab.java
+++ AboutTab.java
@@ -2,39 +2,22 @@
  * This is everything to do with the about tab of the GUI
  */
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-
 
 public class AboutTab extends mainFrame implements ActionListener
 {	
-	JButton about; //about button
-	JLabel info;
-	JTextArea desc;
-	boolean created = false;
-
 	AboutTab()
 	{
-		//basic setup for the about button
-		about = new JButton();
-		about = aboutButton;
-		
+		//setup for the aboutButton
 		aboutButton.setBounds(0,0,200,100); //(x, y, width, height)
 		aboutButton.addActionListener(this);
 		aboutButton.setFocusable(false);
-
 		aboutButton.setText("About");	
 		
 		//basic setup for the about panel
-
 		aboutPanel.setBounds(200,0,500,500);
-		aboutPanel.setVisible(false);
-		
-		
+		aboutPanel.setVisible(false);	
 	}
 
 	/*
@@ -42,7 +25,7 @@ public class AboutTab extends mainFrame implements ActionListener
 	 */
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource() == about)
+		if(e.getSource() == aboutButton)
 		{
 			/*
 			 * This section changes the visibility of the different panels accordingly, 
@@ -53,25 +36,7 @@ public class AboutTab extends mainFrame implements ActionListener
 			visualizePanel.setVisible(false);
 			scrollPane.setVisible(false);
 			
-			
-			if(created == false)
-			{
-				desc = new JTextArea("CREATED BY:\n" +
-					    "Team #42\n" +
-					    "Alexis Correa\n" +
-					    "Michael Moore\n" +
-					    "Anh Pham\n" +
-					    "Thi Viet Chinh Ngo"
-					);
-				desc.setLineWrap(false);
-				desc.setWrapStyleWord(true);
-				desc.setEditable(false);
-				aboutPanel.add(desc);
-				created = true;
-			}
-			
-			
-		}
-
+		}		
 	}
 }
+
