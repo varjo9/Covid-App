02d05fe1dbd58262bd337d98a3126ca5db1cb167
varjo9 <79824903+varjo9@users.noreply.github.com>
2021-04-10 15:16:31 -0700

    Update mainFrame.java

--- mainFrame.java
+++ mainFrame.java
@@ -3,6 +3,7 @@
  * The frame does not have a layout, elements are placed manually
  */
 
+
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JButton;
@@ -10,22 +11,44 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.text.DecimalFormat;
+
 
 public class mainFrame 
 {
+	//frame + all of the panels that will contain content and the table
 	public static JFrame frame;
 	public static JPanel aboutPanel;
 	public static JPanel addPanel;
 	public static JPanel visualizePanel;
+	public static JPanel graphPanel;
 	public static JTable table;
 	public static JScrollPane scrollPane;
+	
+	//text area that will be in our about tab
+	public static JTextArea desc;
 
+	//where we'll store all of the data needed for our table + graphs
 	static String[] columnNames = {"ID","Last Name","First Name", "Vaccine Type", "Vaccine Date", "Vaccine Location"}; //table's columns
 	static String[][] data = {}; //rest of tables data
+	
+	//labels in our add panel
 	private static JLabel idLabel;
 	private static JLabel lastNameLabel;
 	private static JLabel firstNameLabel;
@@ -33,11 +56,7 @@ public class mainFrame
 	private static JLabel vDateLabel;
 	private static JLabel vLocationLabel;
 	
-	public static JButton aboutButton;
-	public static JButton loadButton;
-	public static JButton addButton;
-	public static JButton saveButton;
-	public static JButton visualizeButton;
+	//textfields in our add panel
 	public static JTextField idInput;
 	public static JTextField lastNameInput;
 	public static JTextField firstNameInput;
@@ -45,8 +64,33 @@ public class mainFrame
 	public static JTextField vDateInput;
 	public static JTextField vLocationInput;
 	
+	//submit button in our add panel
 	public static JButton submitButton;
 	
+	//all of the buttons on the side that will let us switch between panels
+	public static JButton aboutButton;
+	public static JButton loadButton;
+	public static JButton addButton;
+	public static JButton saveButton;
+	public static JButton visualizeButton;
+	
+	//the two buttons in the visualize panel that will allow us to switch between graphs
+	public static JButton barButton;
+	public static JButton pieButton;
+		
+	//needed to create bar chart
+	public static DefaultCategoryDataset barChartData;
+	public static JFreeChart barChart;
+	public static CategoryPlot barChartPlot;
+	public static ChartPanel barPanel;
+	
+	//needed to create pie chart
+	public static DefaultPieDataset pieChartData;
+	public static JFreeChart pieChart;
+	public static CategoryPlot pieChartPlot;
+	public static ChartPanel piePanel;
+	
+	@SuppressWarnings("unused")
 	public static void main(String[] args)
 	{	
 		//basic frame setup
@@ -60,7 +104,7 @@ public class mainFrame
 		frame.getContentPane().setLayout(null);
 			
 		/*
-		 * Creates all of the panels (except the scrollPanel, which is added later) and adds 
+		 * Creates all of the panels (except the scrollPanel, which is added later with the table) and adds 
 		 * them to the frame
 		 */
 		aboutPanel = new JPanel();
@@ -74,8 +118,13 @@ public class mainFrame
 		
 		visualizePanel = new JPanel();
 		visualizePanel.setBounds(200, 0, 500, 500);
-		frame.getContentPane().add(visualizePanel);		
+		frame.getContentPane().add(visualizePanel);
 		visualizePanel.setLayout(null);
+		
+		graphPanel = new JPanel();
+		graphPanel.setBounds(0,0,490,450);
+		graphPanel.setLayout(null);
+		visualizePanel.add(graphPanel);
 
 		
 		/*
@@ -95,6 +144,21 @@ public class mainFrame
 		
 		visualizeButton = new JButton();
 		frame.getContentPane().add(visualizeButton);
+		
+		/*
+		 * Here we create the textArea that will be added to the about tab with all of our team information
+		 */
+		desc = new JTextArea("CREATED BY:\n" +
+			    "Team #42\n" +
+			    "Alexis Correa\n" +
+			    "Michael Moore\n" +
+			    "Anh Pham\n" +
+			    "Thi Viet Chinh Ngo"
+			);
+		desc.setLineWrap(false);
+		desc.setWrapStyleWord(true);
+		desc.setEditable(false);
+		aboutPanel.add(desc);
 		
 		/*
 		 * Creates all of the addPanel's labels and adds them to the addPanel
@@ -124,7 +188,7 @@ public class mainFrame
 		addPanel.add(vLocationLabel);
 		
 		/*
-		 * Here we create all of the textFields and add them to the addPanel
+		 * Here we create all of the textFields that accompany the labels and add them to the addPanel
 		 */
 		idInput = new JTextField();
 		idInput.setBounds(255, 125, 100, 20);
@@ -158,7 +222,6 @@ public class mainFrame
 		
 		//submitButton is created and added to the addPanel
 		submitButton = new JButton();
-		
 		addPanel.add(submitButton);
 		
 		/*
@@ -167,16 +230,59 @@ public class mainFrame
 		 */
 		
 		table = new JTable(data, columnNames);
-		table.setBorder(new LineBorder(Color.GREEN, 2));
+		table.setBorder(new LineBorder(Color.GRAY, 1));
 		table.setBackground(Color.WHITE);
 		table.setModel(new DefaultTableModel(data, columnNames));
 		table.setPreferredScrollableViewportSize(new Dimension(500,50));
 		table.setFillsViewportHeight(true);
 			
+			//ScrollPane part
 		scrollPane = new JScrollPane(table);
-		scrollPane.setViewportBorder(new LineBorder(Color.BLUE, 2));
 		scrollPane.setBounds(200, 0, 500, 500);
 		frame.getContentPane().add(scrollPane);
+		
+		/*
+		 * Here we create the two buttons inside the visualize panel that will allow us to switch between the bar char and the pie chart;
+		 */
+		barButton = new JButton();	//bar chart button
+		barButton.setSize(100, 25);
+		barButton.setLocation(125, 462);
+		barButton.setText("Bar Chart");
+		barButton.setFocusable(false);
+		visualizePanel.add(barButton);
+			
+		pieButton = new JButton();	//pie chart button
+		pieButton.setLocation(275, 462);
+		pieButton.setSize(100, 25);
+		pieButton.setText("Pie Chart");
+		pieButton.setFocusable(false);
+		visualizePanel.add(pieButton);
+		
+		/*
+		 * Here we're going to setup everything needed for the bar chart 
+		 */
+		
+		barChartData = new DefaultCategoryDataset();		
+		barChart = ChartFactory.createBarChart("Number of Vaccines Administered per Type", "Vaccine Type", "Number of Doses Administered", barChartData, PlotOrientation.VERTICAL, false, true, false);
+		barChartPlot = barChart.getCategoryPlot();
+		
+		barPanel = new ChartPanel(barChart);
+		barPanel.setBounds(0, 0, 490, 450);
+		graphPanel.add(barPanel);
+		
+		/*
+		 * Here we're going to setup everything needed for the pie chart
+		 */
+		
+		pieChartData = new DefaultPieDataset();
+		pieChart = ChartFactory.createPieChart("Number of Vaccines Administered per Location", pieChartData, true, true, false);
+		PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(  
+		        "{0} : ({1})", new DecimalFormat("0"), new DecimalFormat("0%"));  
+		    ((PiePlot) pieChart.getPlot()).setLabelGenerator(labelGenerator); 
+		    	
+		piePanel = new ChartPanel(pieChart);
+		piePanel.setSize(490, 450);
+		graphPanel.add(piePanel);
 		
 		/*
 		 * Here we initialize all of the classes that allow us to determine actions that need to be taken whenever 
@@ -191,27 +297,17 @@ public class mainFrame
 	}
 	
 	/*
-	 * the following methods are for getting and setting the table data. JTable requires two separate inputs,
-	 * the column data and then the rest of the data, hence the two getters. These two inputs were combined in the setter
-	 * for simplicity.
+	 * the following method is for setting the data table. A JTable requires that you provide the column data and the rest of the data separately, hence the two arguments being passed in
+	 * String[][] data is the actual data, while String[] columns is just the column data.
 	 * 
-	 * Getters are going to be used by the VisualizeTab to retrieve the data that has been loaded in
 	 */
 	
-	public String[][] getData()
-	{
-		return data;
-	}
-	
-	public String[] getColumnNames()
-	{
-		return columnNames;
-	}
-	
+	@SuppressWarnings("static-access")
 	public void setData(String[][] data, String[] columns)
 	{
-		this.data = data;
-		columnNames = columns;
+		this.data = data;	//update our data
+		columnNames = columns;	//update our column data
 		table.setModel(new DefaultTableModel(this.data, columnNames)); //update the table to display the new data
 	}
+	
 }
